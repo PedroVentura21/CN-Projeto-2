7a82395b6c655b083f35cf212a52c3ac5a409806
FelipeVieira9 <felipe.vieiraa52@gmail.com>
2024-11-07 10:32:09 -0300

    removido log

--- resolucoes/q3.1.js
+++ resolucoes/q3.1.js
@@ -8,17 +8,17 @@ const ajusteCurvas_reta = require('../sistemas-lineares/ajusteCurvas_reta');
 // ]
 
 const arr = [
-    {y: Math.log10(2250), x: 1971},
-    {y: Math.log10(3300), x: 1972},
-    {y: Math.log10(6000), x: 1974},
-    {y: Math.log10(29000), x: 1978},
-    {y: Math.log10(134000), x: 1982},
-    {y: Math.log10(275000), x: 1986},
-    {y: Math.log10(1200000), x: 1989},
-    {y: Math.log10(3100000), x: 1993},
-    {y: Math.log10(7500000), x: 1997},
-    {y: Math.log10(9500000), x: 1999},
-    {y: Math.log10(42000000), x: 2000}
+    {y: 2250, x: 1971},
+    {y: 3300, x: 1972},
+    {y: 6000, x: 1974},
+    {y: 29000, x: 1978},
+    {y: 134000, x: 1982},
+    {y: 275000, x: 1986},
+    {y: 1200000, x: 1989},
+    {y: 3100000, x: 1993},
+    {y: 7500000, x: 1997},
+    {y: 9500000, x: 1999},
+    {y: 42000000, x: 2000}
 ]
 const op = [2010, 2020, 2030]
 ajusteCurvas_reta(arr, op);
