45b231473335b89f5c1208c037d41cb8cd765f75
Pedro Ventura <pedro.esmeros@gmail.com>
2024-11-07 09:29:56 -0300

    up

--- resolucoes/q1.2.js
+++ resolucoes/q1.2.js
@@ -2,16 +2,26 @@ const transposta = require("../includes/transposta")
 const input = require("../includes/input")
 const gauss = require("../sistemas-lineares/gauss")
 
-// const A = [
-//     [15, 0.3, 1],
-//     [17, 0.4, 1.2],
-//     [19, 0.55, 1.5]
-// ]
+const A = [
+    [15, 0.3, 1],
+    [17, 0.4, 1.2],
+    [19, 0.55, 1.5]
+]
 
-// const bKg = [3.89, 0.095, 0.282] // matriz coluna da quantidade de elementos em kg
-let matriz = input.q1()
+const bkg = [3.89, 0.095, 0.282] // matriz coluna da quantidade de elementos em kg
 
-let bg = matriz.b.map(m => m*1000)
-let At = transposta(matriz.A)
+// let matriz = input.q1()
+// let bkg = matriz.b
+// let A = matriz.A
 
-console.log(gauss(At, bg))
+let bg = bkg.map(m => m*1000)
+let At = transposta(A)
+
+let x = gauss(At, bg)
+
+console.log(
+    "Poderão ser produzidos por dia\n" +
+    `\tComp. 1: ${x[0].toFixed(0)} unidades\n` +
+    `\tComp. 2: ${x[1].toFixed(0)} unidades\n` +
+    `\tComp. 3: ${x[2].toFixed(0)} unidades`
+)
--- resolucoes/q4.2.js
+++ resolucoes/q4.2.js
@@ -1,15 +1,18 @@
 const Int = require("../sistemas-lineares/integral")
 const input = require("../includes/input")
 
-const entrada = input.q4()
+// const entrada = input.q4()
 
-const x = entrada.X
-const M1 = entrada.M1
-const M2 = entrada.M2
+// const x = entrada.X
+// const M1 = entrada.M1
+// const M2 = entrada.M2
 
-// var x = [0, 10, 20, 30, 40]
-// var M1 = [50.8, 86.2, 136, 72.8, 51]
-// var M2 = [113.6, 144.5, 185, 171.2, 95.3]
+var x = [0, 10, 20, 30, 40]
+var M1 = [50.8, 86.2, 136, 72.8, 51]
+var M2 = [113.6, 144.5, 185, 171.2, 95.3]
 
-console.log(`resultado mét. de Simpson: ${Int.simpson(x, M2)-Int.simpson(x, M1)}`)
-console.log(`resultado mét. dos Trapezios: ${Int.trapezio(x, M2)-Int.trapezio(x, M1)}`)
+console.log(
+    `A área da superfície do rio no intervalo [${x[0]}, ${x[x.length-1]}] é aproximadamente:\n`+
+    `\tPelo mét. de Simpson: ${Int.simpson(x, M2)-Int.simpson(x, M1)} m^2\n` +
+    `\tPelo mét. dos Trapézios: ${Int.trapezio(x, M2)-Int.trapezio(x, M1)} m^2`
+)
